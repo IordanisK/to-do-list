0eb760ac7c946f49a96f5bbd98a5104a15ca3a0d
Dannis Karassavas <dannis.karassavas@cybercom.com>
2022-02-23 12:58:44 +0100

    to-do-list with pagination

---
+++ src/examples/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useMemo } from 'react';
+import Pagination from '../Pagination';
+import data from './data/todos.json';
+import './styles.css';
+
+let PageSize = 10;
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </>
+  );
+}
